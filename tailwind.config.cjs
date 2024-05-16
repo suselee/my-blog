92b11832d792d1ad8c8a601d5ca0d17d5bb62d55
suselee <lyhgreat@gmail.com>
2024-05-16 16:35:18 +0800

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
+        mono: ["Noto Sans SC", "monospace"],
       },
 
       typography: {
