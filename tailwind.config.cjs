352f008f81637b9ae7330cb485f4918c78718ffc
suselee <lyhgreat@gmail.com>
2024-05-16 16:50:37 +0800

    font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["Noto Serif SC", "monospace"],
+        mono: ["Noto Sans SC", "monospace"],
       },
 
       typography: {
